9e46ddc133c2fb4b39561391280a1f07251cc53b
romarioed <109097809+romarioed@users.noreply.github.com>
2022-07-20 23:55:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> 
+<html> 
+<head> 
+<title>PHP-Info example</title> 
+</head> 
+<body> 
+<h2>PHP-info example</h2> 
+<? 
+//Call phpinfo()-function, which prints out the contents of server configuration file. 
+phpinfo(); 
+?> 
+<br /><br /><a href="index.htm">Back to examples</a> 
+</body> 
+</html>
